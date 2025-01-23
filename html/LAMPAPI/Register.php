4c60f9accb7b1799be8126c65b25de596dc6dfdb
Jesiel Reyes <jesiel.reyes2002@gmail.com>
2025-01-22 21:01:54 -0500

    found typo in both update and register that was causing error 500

--- html/LAMPAPI/Register.php
+++ html/LAMPAPI/Register.php
@@ -1,42 +1,43 @@
 <?php
-$inData = getRequestInfo();
 
-$firstName = $inData["firstName"];
-$lastName = $inData["lastName"];
-$login = $inData["login"];
-$password = $inData["password"];
+    $inData = getRequestInfo();
 
-$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-if($conn->connect_error)
-{
-    returnWithError($conn->connect_error);
-}
-else
-{
-    $stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
-    $stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
-    $stmt->execute();
-    $id = $conn->insert_id;
-    $stmt->close();
-    $conn->close();
-    returnWithError("");
-}
+    $firstName = $inData["firstName"];
+    $lastName = $inData["lastName"];
+    $login = $inData["login"];
+    $password = $inData["password"];
 
-function getRequestedInfo()
-{
-    return json_decode(file_get_contents('php://input'), true);
-}
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+    if ($conn->connect_error)
+    {
+        returnWithError( $conn->connect_error );
+    }
+    else
+    {
+        $stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
+        $stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+        $stmt->execute();
+        $id = $conn->insert_id;
+        $stmt->close();
+        $conn->close();
+        returnWithError("");
+    }
 
-function sendResultInfoAsJson($obj)
-{
-    header('Content-type: application/json');
-    echo $obj;
-}
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
 
-function returnWithError($err)
-{
-    $retValue = '{"error":"' . $err . '"}';
-    sendResultInfoAsJson($retValue);
-}
+    function sendResultInfoAsJson( $obj )
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError( $err )
+    {
+        $retValue = '{"error":"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+    }
 
 ?>
--- html/LAMPAPI/UpdateContact.php
+++ html/LAMPAPI/UpdateContact.php
@@ -23,7 +23,7 @@ else
     returnWithError("");
 }
 
-function getRequestedInfo()
+function getRequestInfo()
 {
     return json_decode(file_get_contents('php://input'), true);
 }
