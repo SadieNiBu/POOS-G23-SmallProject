023f277ef1461159dfdcc563bd1b0fb54f04c48a
Jesiel Reyes <jesiel.reyes2002@gmail.com>
2025-01-30 08:20:20 -0500

    forget to inlcude UpdateContact file

--- html/LAMPAPI/UpdateContact.php
+++ html/LAMPAPI/UpdateContact.php
@@ -1,43 +1,43 @@
 <?php
 
-$inData = getRequestInfo();
+    $inData = getRequestInfo();
 
-$firstName = $inData["firstName"];
-$lastName = $inData["lastName"];
-$phone = $inData["phone"];
-$email = $inData["email"];
-$ID = $inData["ID"];
+    $firstName = $inData["firstName"];
+    $lastName = $inData["lastName"];
+    $phone = $inData["phone"];
+    $email = $inData["email"];
+    $ID = $inData["ID"];
 
-$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-if ($conn->connect_error)
-{
-    returnWithError($conn->connect_error);
-}
-else
-{
-    $stmt = $conn->prepare("UPDATE Contacts set FirstName = ?, LastName = ?, Phone = ?, Email = ? where ID = ?");
-    $stmt->bind_param("ssssi", $firstName, $lastName, $phone, $email, $ID);
-    $stmt->execute();
-    $stmt->close();
-    $conn->close();
-    returnWithError("");
-}
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+    if ($conn->connect_error)
+    {
+        returnWithError($conn->connect_error);
+    }
+    else
+    {
+        $stmt = $conn->prepare("UPDATE Contacts set FirstName = ?, LastName = ?, Phone = ?, Email = ? where ID = ?");
+        $stmt->bind_param("ssssi", $firstName, $lastName, $phone, $email, $ID);
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();
+        returnWithError("");
+    }
 
-function getRequestInfo()
-{
-    return json_decode(file_get_contents('php://input'), true);
-}
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
 
-function sendResultInfoAsJson($obj)
-{
-    header('Content-type: application/json');
-    echo $obj;
-}
+    function sendResultInfoAsJson($obj)
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
 
-function returnWithError($err)
-{
-    $retValue = '{"error":"' . $err . '"}';
-    sendResultInfoAsJson($retValue);
-}
+    function returnWithError($err)
+    {
+        $retValue = '{"error":"' . $err . '"}';
+        sendResultInfoAsJson($retValue);
+    }
 
 ?>
