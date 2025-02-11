e47eae996f5642865659988994a2e0f073fed8c6
Stanley Yang <gellal9991@gmail.com>
2025-02-10 19:46:43 -0500

    Another attempt at fixing delete contact popup

--- html/js/code.js
+++ html/js/code.js
@@ -1205,6 +1205,6 @@ function emptyCarouselCard() {
 	cardToEmpty.querySelector('h2').textContent = '';
     cardToEmpty.querySelector('p:nth-child(2)').textContent = '';
     cardToEmpty.querySelector('p:nth-child(3)').textContent = '';
-    cardToEmpty.querySelector('edit-card').innerHTML = '';
-	cardToEmpty.querySelector('delete-card').innerHTML = '';
+    cardToEmpty.querySelector('.edit-card').innerHTML = '';
+	cardToEmpty.querySelector('.delete-card').innerHTML = '';
 }
